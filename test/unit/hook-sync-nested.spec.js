b03c2c04a07facfe7ff78663e48f39ba509d7d10
Christopher Hiller <boneskull@boneskull.com>
2018-04-12 16:27:42 -0700

    convert test/unit/hook-sync-nested.spec.js to unexpected
    Signed-off-by: Christopher Hiller <boneskull@boneskull.com>

--- test/unit/hook-sync-nested.spec.js
+++ test/unit/hook-sync-nested.spec.js
@@ -19,7 +19,7 @@ describe('serial', function () {
     });
 
     it('foo', function () {
-      expect(calls).to.eql([
+      expect(calls, 'to equal', [
         'parent before',
         'parent before test foo'
       ]);
@@ -27,7 +27,7 @@ describe('serial', function () {
     });
 
     it('bar', function () {
-      expect(calls).to.eql([
+      expect(calls, 'to equal', [
         'parent before',
         'parent before test foo',
         'foo',
@@ -47,7 +47,7 @@ describe('serial', function () {
       });
 
       it('one', function () {
-        expect(calls).to.eql([
+        expect(calls, 'to equal', [
           'parent before',
           'parent before test foo',
           'foo',
@@ -66,7 +66,7 @@ describe('serial', function () {
       });
 
       it('two', function () {
-        expect(calls).to.eql([
+        expect(calls, 'to equal', [
           'parent before',
           'parent before test foo',
           'foo',
